0b7053d21a6f648921f4a50764096e7ef357fba3
christianoquintela <christianoquintela@gmail.com>
2025-12-22 17:23:28 -0300

    Atualizando UserSS

--- HELP.md
+++ HELP.md
@@ -4,3 +4,6 @@
 
 Vídeo auxiliar na config do security:
 https://www.youtube.com/watch?v=AlIxmv6smWk
+
+https://www.youtube.com/watch?v=zYSFurMvT54&list=PLiXotHlANc8ptwP6wajo73OZo9Nh5i597&index=23
+aula 20.
---
+++ src/main/java/com/example/training/security/UserSpringSecurity.java
@@ -0,0 +1,55 @@
+package com.example.training.security;
+
+import com.example.training.models.enums.ProfileENUM;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UserSpringSecurity implements UserDetails {
+
+    private Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSpringSecurity(Long id, String username, String password, Set<ProfileENUM> profileENUMS) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = profileENUMS.stream().map(x -> new SimpleGrantedAuthority(x.getDescription())).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean hasRole(ProfileENUM profileENUM){
+        return getAuthorities().contains(new SimpleGrantedAuthority(profileENUM.getDescription()));
+    }
+}
